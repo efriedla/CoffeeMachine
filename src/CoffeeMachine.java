5efa27653d411cfb4a615593af15c45e58f07fb9
moocow <efriedla20852@gmail.com>
2021-04-13 09:54:29 -0400

    Stage 4 alter inventory based on coffee ordered

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -21,34 +21,66 @@ public class CoffeeMachine {
 	//coffee recipe
 	int beansNeeded = 15;//g
 	int milkNeeded = 50;//ml
-	int waterNeeded = 200; //ml
+	int waterNeeded; //ml
 	int cost = 1;
 
 
-	CoffeeMachine(int cups){
+	CoffeeMachine(int cups, int coffeeType ){
 		this.cupsToMake = cups;
+		drinkTypeInstructions(coffeeType);
 		canMake();
+
 	}
 
 	//Determine drink
-	public static void DrinkTypeInstructions(){
-		// espresso, water = 250, coffee beans =  16 cost =  $4
-		// latte, water = 350 ml , milk 75 ml,  beans = 20 g, costs $7
-		// cappuccino, water = 200 ml, milk = 100 ml, beans = 12 g . It costs $6
-		// coffee, water = 200, milk = 50, beans = 15 cost = 1
+	public void drinkTypeInstructions(int coffeeType){
+		switch (coffeeType){
+			case 1:
+				// espresso, water = 250, coffee beans =  16 cost =  $4
+				waterNeeded = 250;
+				beansNeeded = 16;
+				milkNeeded = 0;
+				cost = 4;
+				System.out.println("Espresso");
+				break;
+			case 2:
+				// latte, water = 350 ml , milk 75 ml,  beans = 20 g, costs $7
+				this.waterNeeded = 350;
+				beansNeeded = 20;
+				milkNeeded = 75;
+				cost = 7;
+				System.out.println("Latte");
+				break;
+			case 3:
+				// cappuccino, water = 200 ml, milk = 100 ml, beans = 12 g . It costs $6
+				waterNeeded = 200;
+				beansNeeded = 12;
+				milkNeeded = 100;
+				cost = 6;
+				System.out.println("Cappuccino");
+				break;
+			default:
+				// coffee, water = 200, milk = 50, beans = 15 cost = 1
+				waterNeeded = 200;
+				beansNeeded = 15;
+				milkNeeded = 50;
+				cost = 1;
+				System.out.println("Coffee");
+		}
+
 
 	}
 
 
 	// ingrediance by Coffee
 	public  int getWater(int cups ){
-		return cups * this.waterNeeded;
+		return cups * waterNeeded;
 	}
 	public  int getMilk(int cups){
-		return cups * this.milkNeeded;
+		return cups * milkNeeded;
 	}
 	public  int getBeans(int cups){
-		return cups * this.beansNeeded;
+		return cups * beansNeeded;
 	}
 
 
@@ -56,6 +88,10 @@ public class CoffeeMachine {
 		while(this.waterInventoryLeft && this.milkInventoryLeft && this.beansInventoryLeft && disposableCupsLeft ){
 			this.cupsMade ++;
 			this.waterInventoryLeft = (this.waterInventory - getWater(this.cupsMade) >= 0) ? true:false;
+//			System.out.println("Water inventory "+  milkInventory);
+//			System.out.println("Water inventory - "+  getMilk(this.cupsMade));
+//			System.out.println("Water needed "+ this.milkNeeded);
+//			System.out.println("Cups of coffee " + cupsMade);
 			this.milkInventoryLeft = (this.milkInventory - getMilk(this.cupsMade) >= 0) ? true:false;
 			this.beansInventoryLeft = (this.beanInventory - getBeans(this.cupsMade) >= 0) ? true:false;
 			disposableCupsLeft = (disposableCups - cupsMade >= 0) ? true:false;
@@ -64,7 +100,15 @@ public class CoffeeMachine {
 		if(cupsMade < cupsToMake ){
 			System.out.println("No, I can make only "+ cupsMade +" cups of coffee");
 		}else{
-			System.out.println("Yes, I can make that amount of coffee");
+			//set inventory
+			waterInventory -= getWater(this.cupsMade);
+			milkInventory -= getMilk(this.cupsMade);
+			beanInventory -= getBeans(this.cupsMade);
+			System.out.println("Yes, I can make that amount of coffee " + cupsMade + "\nWater inventory" +
+					" " + waterInventory);
+			System.out.println("Milk inventory " + milkInventory);
+			System.out.println("Bean inventory " + beanInventory);
+
 		}
 	}
 
--- src/main.java
+++ src/main.java
@@ -10,90 +10,10 @@ public class main {
 
 
 	public static void main(String[] args) {
-		//Stage 1
-		//coffeeProcess();
 
-		//Stage 2
-//		coffeeDrinksIngredianeNeeded(25);
-
-		//Stage 3
-//		ableToMakeCoffee(3);
-
-		//stage 3.5
-		CoffeeMachine order1 = new CoffeeMachine(2);
+		//stage 4
+		CoffeeMachine order1 = new CoffeeMachine(2, 2);
 	}
 
-	/**
-	 * Stage 1: print out coffee process
-	 */
-	public static void coffeeProcess(){
-		System.out.println("Starting to make a coffee\n" +
-				"\n" +
-				"Grinding coffee beans\n" +
-				"\n" +
-				"Boiling water\n" +
-				"\n" +
-				"Mixing boiled water with crushed coffee beans\n" +
-				"\n" +
-				"Pouring coffee into the cup\n" +
-				"\n" +
-				"Pouring some milk into the cup\n" +
-				"\n" +
-				"Coffee is ready!");
-	}
 
-	/**
-	 * Displays how much of each ingrediant need to max the mount of cups of coffee
-	 * @param cups
-	 */
-	public static void coffeeDrinksIngredianeNeeded(int cups){
-		System.out.println("For " + cups + " Cups of coffee you will need");
-		final int coffeeBeansNeeded = 15;//g
-		final int coffeeMilkNeeded = 50;//ml
-		final int coffeeWaterNeeded = 200; //ml
-		//200 ml of water, 50 ml of milk, and 15 g of coffee beans.
-		System.out.println("Milk needed " + cups * coffeeMilkNeeded);
-		System.out.println("Water needed " + cups * coffeeWaterNeeded);
-		System.out.println("Beans needed " + cups * coffeeBeansNeeded);
-
-	}
-	public static int getWater(int cups){
-		return cups * 200;
-	}
-	public static int getMilk(int cups){
-		return cups * 50;
-	}
-	public static int getBeans(int cups){
-		return cups * 15;
-	}
-
-	public static void ableToMakeCoffee(int cups){
-		int waterInventory = 400;
-		int beanInventory = 15;
-		int milkInventory = 50;
-		int cupsMade = 0;
-
-
-		boolean waterInventoryLeft = true;
-		boolean milkInventoryLeft = true;
-		boolean beansInventoryLeft = true;
-
-
-		while(waterInventoryLeft && milkInventoryLeft && beansInventoryLeft){
-			cupsMade ++;
-			waterInventoryLeft = (waterInventory - getWater(cupsMade) >= 0) ? true:false;
-			milkInventoryLeft = (milkInventory - getMilk(cupsMade) >= 0) ? true:false;
-			beansInventoryLeft = (beanInventory - getBeans(cupsMade) >= 0) ? true:false;
-
-		}
-		//System.out.println(cupsMade -= 1);
-		if(cupsMade < cups ){
-			System.out.println("No, I can make only "+ cupsMade +" cups of coffee");
-		}else{
-			System.out.println("Yes, I can make that amount of coffee");
-		}
-
-
-
-	}
 }
